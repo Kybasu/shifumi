381dcb3620179bd97baad692f8566e5eb061fc4e
Flavien Chagras <flavien.chagras@protonmail.com>
2022-05-17 11:26:39 +0200

    test(fix): test get sign

--- tests/GameTest.php
+++ tests/GameTest.php
@@ -8,6 +8,6 @@ final class GameTest extends TestCase
 {
     public function test_getSign(): void {
         $sign = Game::getSign();
-        $this->assertContains($sign, ['1', '2', '3']);
+        $this->assertContains($sign, [1, 2, 3]);
     }
 }
