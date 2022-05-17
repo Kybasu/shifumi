d94c4b63d90be27abc96f2c3c36bbc39ff4daf5e
Flavien Chagras <flavien.chagras@protonmail.com>
2022-05-17 11:34:53 +0200

    test(app): test play

--- tests/GameTest.php
+++ tests/GameTest.php
@@ -21,4 +21,91 @@ final class GameTest extends TestCase
         $sign = Game::translateSign(random_int(4, 100));
         $this->assertEquals('', $sign);
     }
+
+    public function test_play() {
+        $rock = 1;
+        $paper = 2;
+        $scissors = 3;
+
+        $round = Game::play($rock, $rock);
+        $this->assertEquals([
+            'result' => 'Égalité',
+            'signs' => [
+                'Jules' => $rock,
+                'Flavien' => $rock,
+            ]
+        ], $round);
+
+        $round = Game::play($rock, $paper);
+        $this->assertEquals([
+            'result' => 'Flavien',
+            'signs' => [
+                'Jules' => $rock,
+                'Flavien' => $paper,
+            ]
+        ], $round);
+
+        $round = Game::play($rock, $scissors);
+        $this->assertEquals([
+            'result' => 'Jules',
+            'signs' => [
+                'Jules' => $rock,
+                'Flavien' => $scissors,
+            ]
+        ], $round);
+
+        $round = Game::play($paper, $paper);
+        $this->assertEquals([
+            'result' => 'Égalité',
+            'signs' => [
+                'Jules' => $paper,
+                'Flavien' => $paper,
+            ]
+        ], $round);
+
+        $round = Game::play($paper, $rock);
+        $this->assertEquals([
+            'result' => 'Jules',
+            'signs' => [
+                'Jules' => $paper,
+                'Flavien' => $rock,
+            ]
+        ], $round);
+
+        $round = Game::play($paper, $scissors);
+        $this->assertEquals([
+            'result' => 'Flavien',
+            'signs' => [
+                'Jules' => $paper,
+                'Flavien' => $scissors,
+            ]
+        ], $round);
+
+        $round = Game::play($scissors, $scissors);
+        $this->assertEquals([
+            'result' => 'Égalité',
+            'signs' => [
+                'Jules' => $scissors,
+                'Flavien' => $scissors,
+            ]
+        ], $round);
+
+        $round = Game::play($scissors, $rock);
+        $this->assertEquals([
+            'result' => 'Flavien',
+            'signs' => [
+                'Jules' => $scissors,
+                'Flavien' => $rock,
+            ]
+        ], $round);
+
+        $round = Game::play($scissors, $paper);
+        $this->assertEquals([
+            'result' => 'Jules',
+            'signs' => [
+                'Jules' => $scissors,
+                'Flavien' => $paper,
+            ]
+        ], $round);
+    }
 }
