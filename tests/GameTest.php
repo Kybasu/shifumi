b40e80d7b16034cf090837977757e2c04756060e
Jules Sayer <jules.sayer@protonmail.com>
2022-05-17 11:28:11 +0200

    test(sign): added translateSign function test

--- tests/GameTest.php
+++ tests/GameTest.php
@@ -10,4 +10,15 @@ final class GameTest extends TestCase
         $sign = Game::getSign();
         $this->assertContains($sign, [1, 2, 3]);
     }
+
+    public function test_translateSign(): void {
+        $sign1 = Game::translateSign(1);
+        $this->assertEquals('Pierre', $sign1);
+        $sign2 = Game::translateSign(2);
+        $this->assertEquals('Feuille', $sign2);
+        $sign3 = Game::translateSign(3);
+        $this->assertEquals('Ciseaux', $sign3);
+        $sign = Game::translateSign(random_int(4, 100));
+        $this->assertEquals('', $sign);
+    }
 }
